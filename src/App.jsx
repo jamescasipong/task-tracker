05274482a97706ee8551c2eea5284b2975b32dbf
Arisuuuuu <casipong.131634@cubao.sti.edu.ph>
2024-09-18 15:29:34 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SignIn from "./components/SignIn";
 import Table from "./components/Table";
 import "./index.css";
 
-let isLocal = true;
+let isLocal = false;
 axios.defaults.baseURL = isLocal ? "http://localhost:3002/api":"https://tasktracker-server.vercel.app/api";
 axios.defaults.withCredentials = true;
 
