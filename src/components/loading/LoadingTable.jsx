f84214d37da54b74a515dec94c7785eba4038f21
Arisuuuuu <jamesxcasipong@gmail.com>
2024-09-30 18:36:57 +0800

    Update LoadingTable.jsx

--- src/components/loading/LoadingTable.jsx
+++ src/components/loading/LoadingTable.jsx
@@ -11,19 +11,7 @@ const LoadingTable = ({ darkMode }) => {
             >
               <div className="animate-pulse bg-gray-300 h-4 w-24 rounded"></div>
             </th>
-            <th
-              scope="col"
-              className="md:block hidden px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              <div className="animate-pulse bg-gray-300 h-4 w-24 rounded"></div>
-            </th>
-            <th
-              scope="col"
-              className="md:block hidden px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              <div className="animate-pulse bg-gray-300 h-4 w-24 rounded"></div>
-            </th>
-
+         
             {/* Add more headers as needed */}
           </tr>
         </thead>
@@ -33,12 +21,7 @@ const LoadingTable = ({ darkMode }) => {
               <td className="px-6 py-4 whitespace-nowrap">
                 <div className="animate-pulse bg-gray-300 h-6 w-full rounded"></div>
               </td>
-              <td className="md:block hidden px-6 py-4 whitespace-nowrap">
-                <div className="animate-pulse bg-gray-300 h-6 w-full rounded"></div>
-              </td>
-              <td className="md:block hidden px-6 py-4 whitespace-nowrap">
-                <div className="animate-pulse bg-gray-300 h-6 w-full rounded"></div>
-              </td>
+              
 
               {/* Add more cells as needed */}
             </tr>
